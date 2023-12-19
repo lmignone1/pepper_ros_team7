99be1ab49d8d031ba81910d6eae9819f52ce64a3
lmignone1 <118554285+lmignone1@users.noreply.github.com>
2023-12-19 18:28:10 +0100

    dialogue_interface refactor (to check)

--- workspace/src/rasa_ros/scripts/dialogue_interface.py
+++ workspace/src/rasa_ros/scripts/dialogue_interface.py
@@ -6,110 +6,119 @@ from std_msgs.msg import Int16MultiArray, String, Int16
 
 from pepper_nodes.srv import Text2Speech, Text2SpeechRequest, Text2SpeechResponse
 
-conversation = False
 
-def rcv_detection(msg):
-    global conversation
+class Speaking():
 
-    # if conversation == False:
-    #     if msg.data == 1:
-    #         conversation = True
-    # elif conversation == True:
-    #     if msg.data == 0:
-    #         conversation = False
-
-
-    if msg.data == 1:
-        conversation = True
-    else:
-        conversation = False
-
-
-def main():
-    rospy.init_node('speaking')
-    print('Node speaking started')
-    rospy.wait_for_service('dialogue_server')
-    dialogue_service = rospy.ServiceProxy('/dialogue_server', Dialogue)
-
-    rospy.wait_for_service('/tts')
-    tts_service = rospy.ServiceProxy('/tts', Text2Speech)
-
-    # terminal = TerminalInterface()
-   
-    rospy.Subscriber('detection', Int16, rcv_detection)
-
-    while not rospy.is_shutdown():
+    def __init__(self) -> None:
+        # inizializzazione del nodo
+        rospy.init_node('pepper_speaking_node')
+        print('Node speaking pepper started')
         
-        print('conversation: ', conversation)
+        # inizializzazione dei servizi
+        rospy.wait_for_service('dialogue_server')
+        self.dialogue_service = rospy.ServiceProxy('/dialogue_server', Dialogue)
 
-        if conversation == True:
-            message = Text2SpeechRequest()
-            message.speech = 'Hello folks'
-            tts_service(message)
-            print('Saluto pr la prima volta')
+        rospy.wait_for_service('/tts')
+        self.tts_service = rospy.ServiceProxy('/tts', Text2Speech)
 
-            i = 0
-            while True:
-                
+        # inizializzazione dei subscriber
+        rospy.Subscriber('detection', Int16, self._rcv_detection)
+
+        # se true allora la persona parla con il robot altrimenti non ci sono persone
+        self.conversation = None
 
 
-                if conversation == False:
-                    print('RESTART:')
-                    restart = DialogueRequest()
-                    restart.input_text = '/restart'
-                    resp = dialogue_service(restart)
-                    print('RISP bot al restart', resp.answer)
-                    break
-                
-                try:
-                    message = rospy.wait_for_message('voice_txt', String, timeout=30) # lunghezza parola + costante 
-                except rospy.ROSException:
-                    message = None
+    def _rcv_detection(self, detected):
+        if detected.data == 1:
+            self.conversation = True
+        else:
+            self.conversation = False
 
-                if message is None:
-                    print('RESTART:')
-                    restart = DialogueRequest()
-                    restart.input_text = '/restart'
-                    resp = dialogue_service(restart)
-                    print('RISP bot al restart', resp.answer)
-                    break
-
-                message = message.data.lower()
-
-                if message == 'exit': 
-                    break
-                
-                try:
-            
-                    print("[IN]:", message)
-                    bot_request = DialogueRequest()
-                    bot_request.input_text = message
-                    
-                    bot_answer = dialogue_service(bot_request)
-                    
-                    print("[OUT]:", bot_answer.answer)
-
-                    msg = Text2SpeechRequest()
-                    msg.speech = bot_answer.answer
-                    
-                    tts_service(msg)
-
-                    print('fine ciclo')
-                
-                except rospy.ServiceException as e:
-                    print("Service call failed: %s"%e)
-                
-                i += 1
-                print('fine iterazione ', i)
-                
-            print('sono fuori dall iterazione')
-
-            
+    def _make_request(self, text, request_obj):
+        if isinstance(request_obj, DialogueRequest):
+            request_obj.input_text = text
+            return request_obj
         
-        rospy.sleep(1)
+        if isinstance(request_obj, Text2SpeechRequest):
+            request_obj.speech = text
+            return request_obj
+        
+
+        
+
+
+    def start(self):
+        while not rospy.is_shutdown():
+        
+            print('conversation: ', self.conversation)
+
+            if self.conversation == True:
+                speech_req = self._make_request('Hello folks', Text2SpeechRequest())
+                self.tts_service(speech_req)
+                print('Saluto pr la prima volta')
+
+                i = 0
+                while True:
+                
+                    if self.conversation == False:
+                        print('RESTART:')
+                        restart_req = self._make_request('/restart', DialogueRequest())
+                        resp = self.dialogue_service(restart_req)
+                        print('RISP bot al restart', resp.answer)
+                        break
+                    
+                    try:
+                        user_message = rospy.wait_for_message('voice_txt', String, timeout=TIMEOUT) # lunghezza parola + costante 
+                    except rospy.ROSException:
+                        print('RESTART:')
+                        restart_req = self._make_request('/restart', DialogueRequest())
+                        resp = self.dialogue_service(restart_req)
+                        print('RISP bot al restart', resp.answer)
+                        break
+
+                    # if message is None:
+                    #     print('RESTART:')
+                    #     restart_req = DialogueRequest()
+                    #     restart_req.input_text = '/restart'
+                    #     resp = self.dialogue_service(restart_req)
+                    #     print('RISP bot al restart', resp.answer)
+                    #     break
+
+                    user_message = user_message.data.lower()
+
+                    if user_message == 'exit': 
+                        break
+                    
+                    try:
+                
+                        print("[IN]:", user_message)
+                        user_req = self._make_request(user_message.data.lower(), DialogueRequest())
+                        
+                        bot_answer = self.dialogue_service(user_req)
+                        
+                        print("[OUT]:", bot_answer.answer)
+
+                        bot_answer = self._make_request(bot_answer.answer, Text2SpeechRequest())
+                        self.tts_service(bot_answer)
+
+                        print('fine ciclo')
+                    
+                    except rospy.ServiceException as e:
+                        print("Service call failed: %s"%e)
+                    
+                    i += 1
+                    print('fine iterazione ', i)
+                    
+                print('sono fuori dall iterazione')
+
+            rospy.sleep(1)
+
 
 if __name__ == '__main__':
+    TIMEOUT = 30
+
     try: 
-        main()
+        speaking = Speaking()
+        speaking.start()
     except rospy.ROSInterruptException:
         pass
